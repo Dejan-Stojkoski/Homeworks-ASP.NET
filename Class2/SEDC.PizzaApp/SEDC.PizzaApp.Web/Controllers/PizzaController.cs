35ac1d64b05c26c359a0d60842ae9e166df91976
Dejan-Stojkoski <dejan_pp2005@live.com>
2021-08-09 14:03:22 +0200

    Class 2 homework upload

---
+++ Class2/SEDC.PizzaApp/SEDC.PizzaApp.Web/Controllers/PizzaController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Web.Models;
+
+namespace SEDC.PizzaApp.Web.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Pizza> pizzas = StaticDB.Pizzas;
+
+            return new JsonResult(pizzas);
+        }
+
+        public IActionResult Details(int id)
+        {
+            if (StaticDB.Pizzas.Any(x => x.Id == id))
+            {
+                //return View();
+
+                Pizza pizza = StaticDB.Pizzas.SingleOrDefault(x => x.Id == id);
+
+                return new JsonResult(pizza);
+            }
+            else
+            {
+                //return new EmptyResult();
+
+                return RedirectToAction("Error", "Home");
+            }
+        }
+    }
+}
---
+++ Class2/SEDC.PizzaApp/SEDC.PizzaApp.Web/Models/Pizza.cs
@@ -0,0 +1,9 @@
+﻿namespace SEDC.PizzaApp.Web.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsOnPromotion { get; set; }
+    }
+}
